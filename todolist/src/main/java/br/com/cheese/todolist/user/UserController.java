908e386c9bec049349cffe7d4c8c5a0b784deaa2
Henderson Dias <84034041+xCheese00@users.noreply.github.com>
2023-10-14 22:38:40 -0300

    Add files via upload

---
+++ todolist/src/main/java/br/com/cheese/todolist/user/UserController.java
@@ -0,0 +1,61 @@
+package br.com.cheese.todolist.user;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
+/*
+ * Modificadores
+ * public
+ * private
+ * protected
+ */
+@RestController
+@RequestMapping("/users")
+
+public class UserController {
+
+        /*
+         * String
+         * Interger (int) numeros inteiros
+         * Double (Double) numeros 0.0000
+         * float numeros 0.000
+         * char AC
+         * date (data)
+         * void
+         */
+
+         /*
+          * body
+          */
+    @Autowired
+    private IUserRepository userRepository;
+
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+        
+
+        if(user != null){
+            // Mensagem de erro
+            // Status code
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe.");
+        }
+        var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.OK).body(userCreated);
+    }
+    
+}
